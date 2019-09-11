ec74be39b4a50cb63f78ad9ba63557f685211a4f
chayapol <meekerd@chayapols-MacBook-Pro.local>
2019-09-11 10:51:25 +0700

    axios users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export const ADD_USERS = 'ADD_USERS'
 
-export const addUsers = async () => {
+export const addUsers = () => async dispatch => {
     try {
         const result = await axios.get('https://jsonplaceholder.typicode.com/users')
         return ({
