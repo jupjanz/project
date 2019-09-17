73370dfc359b14b8234998926d7df7c027768f49
chayapol <meekerd@chayapols-MacBook-Pro.local>
2019-09-17 11:41:15 +0700

    renameunsafe

--- src/Map/MapWithASearchBox.js
+++ src/Map/MapWithASearchBox.js
@@ -17,7 +17,7 @@ const MapWithASearchBox = compose(
     mapElement: <div style={{ height: `100%` }} />,
   }),
   lifecycle({
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
       const refs = {}
       this.setState({
         bounds: null,
